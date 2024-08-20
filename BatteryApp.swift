48aca16eef37d539f1eae44c23ff2c9ad152e02f
Han Yu Foong <117330435+IntelAntique@users.noreply.github.com>
2024-08-20 16:37:35 -0500

    First Commit

---
+++ BatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryApp.swift
+//  Battery
+//
+//  Created by HAN YU FOONG on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct BatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
