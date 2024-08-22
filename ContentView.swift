7f9d94c68f9ef5c8b3e110ac667c1f58fa43323b
Han Yu Foong <117330435+IntelAntique@users.noreply.github.com>
2024-08-22 18:35:13 -0500

    Update ContentView.swift
    Notification prototype

--- ContentView.swift
+++ ContentView.swift
@@ -10,13 +10,49 @@ import UIKit
 import UserNotifications
 
 struct ContentView: View {
+    var status: [String] = ["Idle", 
+                            "Charging",
+                            "Charged"]
+    @State private var allowed: BooleanLiteralType = false
+    
+    func location() { }
+    func notified() {
+        if(allowed == false){
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                if success {
+                    print("All set!")
+                    allowed = true
+                } else if let error {
+                    print(error.localizedDescription)
+                    return
+                }
+            }
+        }
+        let content = UNMutableNotificationContent()
+        content.title = "Check Battery"
+        content.subtitle = "Battery advice from a friend"
+        content.sound = UNNotificationSound.default
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+        UNUserNotificationCenter.current().add(request)
+    }
+    
     var body: some View {
         VStack{
             HStack{
-                Text(UIDevice.current.name)
+                Menu(UIDevice.current.name) {
+                    Button("Location", action: location)
+                    Button("Notify", action: notified)
+                    Text("Status: " + status[0])
+                }
 //                Text(UUID().uuidString)
                 Image(systemName: "battery.50")
-//                    .symbolEffect(.variableColor.reversing)
+                Image(systemName: "wifi")
+//                    .symbolEffect(.bounce, value: 1)
+                    .symbolEffect(.variableColor.reversing)
             }
             Divider()
         }
