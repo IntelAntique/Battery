a2b361cacfc698a79e17a9cb3bd906102469b65c
Han Yu Foong <117330435+IntelAntique@users.noreply.github.com>
2024-08-24 17:50:06 -0500

    Added MapView

---
+++ MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  BatteryCheck
+//
+//  Created by HAN YU FOONG on 8/24/24.
+//
+
+import Foundation
+import MapKit
+import SwiftUI
+
+struct MapView: View {
+    let manager = CLLocationManager()
+    @State private var cam: MapCameraPosition = .userLocation(fallback: .automatic)
+
+    var body: some View {
+        ZStack{
+            Map(position: $cam){
+                UserAnnotation()
+            }
+            .onAppear(){
+                manager.requestWhenInUseAuthorization()
+            }
+            .mapControls{
+                MapUserLocationButton()
+            }
+        }
+    }
+}
